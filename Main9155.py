b87ea0145dc24aed4dd7cdb5559262bab695fbb6
wlsgnl <ican243@gmail.com>
2026-03-24 11:57:02 +0900

    ''' 활용

---
+++ Main9155.py
@@ -0,0 +1,8 @@
+print('My second program')
+print(2)
+print('programs')
+print()
+print()
+print('''my second program
+2
+programs''')
--- practice.py
+++ practice.py
@@ -1,24 +1,28 @@
-print('hello world')
+print('hello world') #자기소개 
 print('나는 임진휘')
 print("공부를 시작했다")
-print('----------연산자------------')
+print('----------연산자------------') #연산자 활용
 print(5+2)
 print(5-2)
 print(5*2)
 print(5/2)
 print(5%2)
-print('------------비교 연산자---------')
+print('------------비교 연산자---------') #비교 연산자 활용 
 print(5>2)
 print(5>=2)
 print(5<2)
 print(5<=2)
 print(5 == 2)
 print(5 != 2)
+
 print()
+
 print(3<5 and 7<5)
 print(3<5 or 7<5)
 print(not 3<5)
+
 print()
+
 print('c' in 'cat')
 print('c' not in 'cat')
 print('--------------sep, enumerate 활용 -------------')
@@ -31,6 +35,7 @@ list(enumerate(seasons))
 for idx, val in enumerate(seasons):
     print(idx, val)
 print()
+'''
 name = '임진휘'
 print(name)
 print()
@@ -39,4 +44,4 @@ print(name)
 print()
 age = int(input("나이입력: "))
 print = (age + 1)
-
+'''
