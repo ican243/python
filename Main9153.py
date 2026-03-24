2ae99ad9364d617d7ecc9f5ef8873d560e4ffeaf
wlsgnl <ican243@gmail.com>
2026-03-24 11:01:09 +0900

    first commit

---
+++ Main9153.py
@@ -0,0 +1,2 @@
+print("Hello Jungol!")
+print("Hi Python")
---
+++ practice.py
@@ -0,0 +1,20 @@
+print('hello world')
+print('나는 임진휘')
+print("공부를 시작했다")
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+
+name = '임진휘'
+print(name)
+
+name = input("이름입력: ")
+print(name)
+
+Age = input("나이입력: ")
+print = (Age)
+
+age = int(input("나이입력: "))
+print = (age + 1)
+
