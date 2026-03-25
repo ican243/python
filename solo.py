067f4225fd037ccaa5c3e89b4600d37afbf4702b
wlsgnl <ican243@gmail.com>
2026-03-25 18:56:11 +0900

    연산자 활용 문제 풀이 완료

--- solo.py
+++ solo.py
@@ -31,13 +31,13 @@ my_tuple = ['오예스', '뭉쉘', '초코파이'] #튜플 활용
 print(my_tuple[1])
 print()
 my_set = {'돈가스', '제육볶음', '보쌈'}
-#print(my_set[0])
-#my_set[1] = '빅파이'
-#my_set.add('닭갈비')
-#my_set.remove('돈가스')
-#my_set.clear()
-#del my_set
-#print(my_set)
+# print(my_set[0])
+# my_set[1] = '빅파이'
+# my_set.add('닭갈비')
+# my_set.remove('돈가스')
+# my_set.clear()
+# del my_set
+# print(my_set)
 print()
 #a = int(7)
 #b = int(3)
@@ -82,7 +82,14 @@ print()
 #f = int(input())
 #g = int(input())
 #print(f"{a+2} {b-2} {c*2} {d/2} {e//2} {f%2} {g**2}")
-a = int(input()) #f-string 사용법
-b = int(input())
-print(f'{a} * {b} = {a*b}')
+#a = int(input()) #f-string 사용법
+#b = int(input())
+#print(f'{a} * {b} = {a*b}')
 
+#a = int(input())
+#print(a)
+#print(a+10)
+
+a = int(input())#input 활용 f-string 활용
+b = int(input())
+print (f'{a-10} + {b*2} = {a-10+b*2}')
