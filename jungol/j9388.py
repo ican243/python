f4d30cf1f0b4bdd4496cfcd195d9ac33d2682353
wlsgnl <ican243@gmail.com>
2026-04-10 18:48:36 +0900

    ORACLE 풀이

--- jungol/j9388.py
+++ jungol/j9388.py
@@ -1,4 +1,11 @@
-N = int(input())
-X = int(input())
+#N = int(input())
+#X = int(input())
 
-print(f'{N:02d}:{X:02d}')
+#print(f'{N:02d}:{X:02d}')
+
+lst = []
+
+for i in range(5):
+    N = input()
+    lst.append(N)
+print(lst)
--- oracledb2.py
+++ oracledb2.py
@@ -1,4 +1,5 @@
 import oracledb
+from person import Person #Model
 
 # 데이터베이스 접속 정보 설정
 dsn = oracledb.makedsn("localhost", 1521, service_name="XE")
@@ -7,22 +8,7 @@ conn = oracledb.connect(user="c##mbc", password="qwer1234", dsn=dsn)
 # 쿼리 실행을 위한 커서 생성
 cursor = conn.cursor()
 
-class Person:
-    def __init__(self, empno, ename, job, mgr, hiredate, sal, comm, deptno):
-        self.empno = empno
-        self.ename = ename
-        self.job = job
-        self. mgr = mgr
-        self.hiredate = hiredate
-        self.sal = sal
-        self.comm = comm
-        self.deptno = deptno
-
-    def print_person(self):
-        print(f'{self.empno} {self.ename} {self.job} {self.mgr} {self.hiredate} {self.sal} {self.comm} {self.deptno}')
-
-
-    
+lst = []
 
 def show_menu():
     print("-- 임직원 관리 시스템 --")
@@ -34,58 +20,75 @@ def show_menu():
     print(menu_num)
     return menu_num
 
-def insert_emp(): 
+def insert_emp():   
     print("새로운 직원의 사번, 이름을 입력하세요....")
     empno, ename = input().split()
     print(empno, ename)
 
-    if empno.isdigit():
+    if empno.isdigit():        
+
         try:
             cursor.execute("INSERT INTO EMP(EMPNO, ENAME) VALUES (:1, :2)", [empno, ename.upper()])
             conn.commit()
             print("Data inserted successfully")
+            lst.clear()
         except oracledb.DatabaseError as e:
             print(f"Error inserting data: {e}")
     else:
-        print('hwi-0001 사번 입력 오류 입니다. 숫자만 입력 가능합니다.')
-        
-def delete_emp():
-    print("삭제할 직원의 번호를 입력하세요...")
+        print("ERR-INSERT-001 : 사번 입력 오류 입니다. 숫자만 입력 가능합니다.")
+
+def delete_emp(): # empno, ename, job, mgr, hiredate, sal, comm, deptno  
+    print("삭제할 직원의 사번을 입력하세요....")
     empno = input()
     print(empno)
-    cursor.execute("DELETE FROM emp WHERE empno = :1", [empno])
-    conn.commit()
 
-    if cursor.rowcount > 0:
-        print("Data deleted successfully")
+    if len(lst) > 0:
+        eq = False
+        for i in lst:
+            # i.print_person()
+            if str(i.empno) == empno:
+                eq = True
+                lst.remove(i)
+        if eq == True:
+            print('해당 사번이 존재합니다.')
+            try:
+                # DELETE FROM EMP WHERE EMPNO='4769'
+                cursor.execute("DELETE FROM EMP WHERE EMPNO=:1", [empno])
+                conn.commit()
+
+                print("Data deleted successfully")
+            except oracledb.DatabaseError as e:
+                print(f"Error inserting data: {e}")
+        else:
+            print("ERR-INSERT-001 : 사번 입력 오류 입니다. 숫자만 입력 가능합니다.")
     else:
-        print("해당 사번이 없습니다.")
+        search_emp()
 
 def search_emp():
-    try:
-        cursor.execute("SELECT * FROM emp")
-
-        for row in cursor:
-            print(row)
-    except oracledb.DatabaseError as e:
-        print(f"Error fetching data: {e}")
-
-def search_emp():                                                           #조회 
+    # print("FUNCTION CALL. search_emp()")
 # SELECT 예제
     try:
         cursor.execute('''
-                SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO
-                FROM emp
-                ORDER BY EMPNO''')
+            SELECT EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO 
+            FROM emp
+            ORDER BY EMPNO''')
         for row in cursor:
-            #print(row)
+            # print(row)
             p = Person(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
-            p.print_person()
+            lst.append(p)
+            # p.print_person()
+
+        for i in lst:
+            i.print_person()
     except oracledb.DatabaseError as e:
         print(f"Error fetching data: {e}")
 
-loop = True
-while loop:
+def search_emp2():
+    # print("FUNCTION CALL. search_emp2()")
+    for i in lst:
+        i.print_person()
+   
+while True:
     select = int(show_menu())
     if select == 1:
         print("1. 직원 추가 메뉴")
@@ -95,10 +98,14 @@ while loop:
         delete_emp()
     elif select == 3:
         print("3. 직원 조회 메뉴")
-        search_emp()
+        print("--- LST SIZE --- ", len(lst))
+        if len(lst) == 0:
+            search_emp()
+        else:
+            search_emp2()
     else:
         print("프로그램 종료 ***")
-        loop = False
+        break
 
 # 커서 및 커넥션 닫기
 cursor.close()
